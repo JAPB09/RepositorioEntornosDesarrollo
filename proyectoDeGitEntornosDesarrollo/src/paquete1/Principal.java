de46739ac0dafc26002a717951aef33282b79479
ACER <ACER@DESKTOP-ET1UMNR>
2025-01-24 16:47:40 +0100

    Commit rama1

--- proyectoDeGitEntornosDesarrollo/src/paquete1/Principal.java
+++ proyectoDeGitEntornosDesarrollo/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola, mundo");
 		System.out.println("Nuevo cambio");
+		System.out.println("Cambio hecho en la rama1");
 		
 	}
 
